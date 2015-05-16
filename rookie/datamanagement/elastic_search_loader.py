ba22d960ad47c76516178a6f1c1d6e5c675144ec
AbeHandler <akh2103@gmail.com>
2015-05-16 18:56:10 -0500

    adding more stuff

--- rookie/datamanagement/elastic_search_loader.py
+++ rookie/datamanagement/elastic_search_loader.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
-
+'''Loads a corpus into elastic search'''
 import glob
 import os
 import json
@@ -7,8 +7,9 @@ import json
 from datetime import datetime
 from elasticsearch import Elasticsearch
 
-class Corpus:
 
+class Corpus(object):
+    '''A corpus of documents in a file system'''
     location = None
 
     def __init__(self, location):
@@ -17,44 +18,51 @@ class Corpus:
         return None
 
     def get_files(self, extension):
-    	globstring = self.location + "/*" + extension
-        return glob.glob(self.location + "/*" + extension)
+        globstring = self.location + "/*" + extension
+        return glob.glob(globstring)
 
 
-#Need a decorator for interface here
-class LensStory: 
+# Need a decorator for interface here
+class LensStory(object):
     '''How do you do an interface in python?'''
     def __init__(self, location):
         filename, file_extension = os.path.splitext(location)
         assert file_extension == ".json"
         self.location = location
-        with (open (location)) as jsonfile:
+        with (open(location)) as jsonfile:
             json_data = json.load(jsonfile)
             # sample timestamp: 2010-10-10T10:10:10
             datestring = json_data['time'].split("T")[0]
             year, month, day = [int(y) for y in datestring.split("-")]
             self.date = str(datetime(year, month, day))
             self.text = json_data['full_text']
-    
+            self.headline = json_data['headline']
+
     def jsonify(self):
+        '''Dump out json'''
         json_output = {}
         json_output['text'] = self.text
         json_output['timestamp'] = self.date
+#        json_output['headline'] = self.headline
         return json.dumps(json_output)
 
 
-es = Elasticsearch(sniff_on_start=True)
+elasticsearch = Elasticsearch(sniff_on_start=True)
 
-es.indices.delete(index='*') # clear out everything from elastic search
+elasticsearch.indices.delete(index='*')  # clear out everything
 
-corpus = Corpus("/backups/lens_corpus/")
+corpus = Corpus("/Users/abramhandler/research/rookie/lens_downloader/files/")
 
 counter = 1
 for story_file in corpus.get_files("*json"):
     story = LensStory(story_file)
-    res = es.index(index="lens", doc_type='news_story', id=counter, body=story.jsonify())
+    res = elasticsearch.index(index="lens",
+                              doc_type='news_story',
+                              id=counter,
+                              body=story.jsonify())
     counter = counter + 1
 
-# to do: add headline
-res = es.search(index="lens", body={"query": {"match_all": {"OPSB"}}})
-print("Got %d Hits:" % res['hits']['total'])
+
+results = elasticsearch.search(index="lens", q="OPSB")
+
+print "Got %d Hits:" % results['hits']['total']
