1085b033f627020d7960237fe192736e2d5641cd
AbeHandler <akh2103@gmail.com>
2015-05-17 07:01:10 -0500

    graph search alost there. saving work/

--- rookie/datamanagement/searcher.py
+++ rookie/datamanagement/searcher.py
@@ -1,11 +1,36 @@
 from elasticsearch import Elasticsearch
+import networkx as nx
+
+G = nx.Graph()
+
+
+def add_node(nid, headline):
+    if nid in G.nodes():
+        G.add_node(int(nid), headline=headline)
+
 
 elasticsearch = Elasticsearch(sniff_on_start=True)
 
-results = elasticsearch.search(index="lens", q="transportation OPSB", size=15)
+results = elasticsearch.search(index="lens", q="OPSB", size=150000)
 
-print len(results['hits']['hits'])
+for result in results['hits']['hits']:
+    headline = result['_source']['headline']
+    add_node(result['_id'], headline)
+    for link in result['_source']['links']:
+        G.add_edge(int(result['_id']), link[1])
 
+node_degress = []
 
-#for hit in results['hits']['hits']:
-#    print("%(timestamp)s %(headline)s %(links)s" % hit["_source"])
+for node in G.nodes():
+    node_degress.append((node, nx.degree(G, node)))
+
+nodes = sorted(node_degress, key=lambda node: node[1], reverse=True)
+
+for node in nodes:
+    print node
+
+# print len(results['hits']['hits'])
+
+# for hit in results['hits']['hits']:
+#    print hit.keys()
+#    print("%(timestamp)s %(id)s %(links)s" % hit["_source"])
