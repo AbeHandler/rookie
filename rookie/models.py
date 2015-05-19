4038bb67fc1c767d726b90cf38d0b1b39d54da7c
AbeHandler <akh2103@gmail.com>
2015-05-18 21:25:35 -0500

    adding models

--- rookie/models.py
+++ rookie/models.py
@@ -1,6 +1,7 @@
 """
 The web app that runs at vault.thelensnola.org/contracts.
 """
+import datetime
 
 from rookie import (
     log
@@ -10,10 +11,10 @@ from elasticsearch import Elasticsearch
 import networkx as nx
 
 
-def add_node(G, nid, headline):
+def add_node(G, nid, headline, timestamp):
     if nid not in G.nodes():
         doc, sentence = nid.split("-")
-        G.add_node(int(doc), headline=headline)
+        G.add_node(int(doc), headline=headline, timestamp=timestamp)
 
 
 class Models(object):
@@ -42,7 +43,8 @@ class Models(object):
 
         for result in results['hits']['hits']:
             headline = result['_source']['headline']
-            add_node(G, result['_id'], headline)
+            timestamp = result['_source']['timestamp']
+            add_node(G, result['_id'], headline, timestamp)
             for link in result['_source']['links']:
                 doc, sentence = result['_id'].split("-")
                 G.add_edge(int(doc), link[1])
@@ -55,14 +57,26 @@ class Models(object):
         nodes = sorted(node_degress, key=lambda node: node[1], reverse=True)
 
         headlines = nx.get_node_attributes(G, 'headline')
+        timestamps = nx.get_node_attributes(G, 'timestamp')
 
         for node in nodes:
             try:
-                item = (node[1], headlines[node[0]])
+                pubdate = timestamps[node[0]].split(" ")[0]
+                year, month, day = pubdate.split("-")
+                pubdate = datetime.date(int(year), int(month), int(day))
+                print pubdate
+                log.info(pubdate)
+                item = (node[1], headlines[node[0]],
+                        str(pubdate))
                 output.append(item)
             except:
                 pass
 
+        output.sort(key=lambda tup: tup[0], reverse=True)
+
+        output = output[0:10]
+
+        output.sort(key=lambda tup: tup[2], reverse=False) # sort by pubdate
         return output
 
     def home(self):
