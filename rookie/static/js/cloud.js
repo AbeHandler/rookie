db4eefc92ef939b53efcedddd78f51797fa37153
AbeHandler <akh2103@gmail.com>
2015-06-05 09:58:08 -0600

    adding stack

--- rookie/static/js/cloud.js
+++ rookie/static/js/cloud.js
@@ -1,14 +1,22 @@
-var data = [4, 8, 15, 16, 23, 42];
+var numbers = window.data.slice(0, 1 + 9);
+
+var width = $("#vis").width();
+
+var max_x = _.max(numbers, function(number){ return number.score; });
+
+max_x = max_x.score;
 
 var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, 420]);
+    .domain([0, max_x])
+    .range([0, 100]);
 
 d3.select("#vis")
   .selectAll("div")
-    .data(data)
+    .data(numbers)
   .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
+    .style("height", function(d) { 
+      return x(d.score) + "px";
+    })
     .style("color", "green")
     .style("background-color", "grey")
-    .text(function(d) { return d; });
+    .text(function(d) { return d.fulltext; });
