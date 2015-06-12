a51071789e88f67ca693ae2c3a8f8a16b2d5a419
AbeHandler <akh2103@gmail.com>
2015-06-12 13:41:27 -0500

    brenocon integration

---
+++ rookie/datamanagement/lensdownloader.py
@@ -0,0 +1,70 @@
+from __future__ import print_function
+
+import urllib2
+import hashlib
+import os
+
+from bs4 import BeautifulSoup
+from rookie import log
+from rookie import corpus_loc
+
+DOMAINLIMITED = "thelensnola.org"
+
+
+def get_page(url):
+    hash_url = hashlib.sha224(url).hexdigest()
+    url_file = corpus_loc + hash_url
+    if os.path.exists(url_file):
+        print("already have " + url)
+        html = "".join([i for i in open(url_file)])
+    else:
+        print("downloading " + url)
+        headers = {'User-Agent': "Abe Handler: urllib2"}
+        req = urllib2.Request(url, headers=headers)
+        con = urllib2.urlopen(req)
+        html = con.read()
+        with open(url_file, 'w') as openfile:
+            print(html.replace("\n", ""), file=openfile)
+    return html
+
+
+def get_html_summary(i):
+    htmls = []
+    i = str(i)
+    url = "http://thelensnola.org/page/" + i + "/?s="
+    htmls.append(get_page(url))
+    log.info('Downloading summary | {}'.format(i))
+    return htmls
+
+
+def get_urls_from_summary(html):
+    urls = []
+    soup = BeautifulSoup(html)
+    h3 = soup.find_all('h3')
+    for i in h3:
+        try:
+            if "Permalink" in i.contents[0].attrs['title']:
+                url = i.contents[0].attrs['href']
+                urls.append(url.strip("/"))
+        except AttributeError:  # can skip elements that don't have hrefs
+            pass
+        except KeyError:  # not a link. does not have a title
+            pass
+    return urls
+
+
+def get_all_urls():
+    url_output = []
+    for i in range(1, 435):
+        summary = get_html_summary(i)
+        for html in summary:
+            urls = get_urls_from_summary(html)
+            for url in urls:
+                url_output.append(url)
+    return url_output
+
+
+if __name__ == '__main__':
+    urls = get_all_urls()
+    for url in urls:
+        page = get_page(url)
---
+++ rookie/datamanagement/lensprocessor.py
@@ -0,0 +1,78 @@
+from __future__ import print_function
+
+import nltk.data
+import hashlib
+
+from datetime import datetime
+from stanford_corenlp_pywrapper import sockwrap
+from bs4 import BeautifulSoup
+from rookie import log
+from rookie import core_nlp_location
+from rookie import processed_location
+from rookie.datamanagement.lensdownloader import get_all_urls
+from rookie.datamanagement.lensdownloader import get_page
+
+proc = sockwrap.SockWrap("ner", corenlp_jars=[core_nlp_location])
+
+domainlimiter = "thelensnola"
+
+ids = {}
+
+counter = 1
+
+
+def get_links(full_text):
+    output = []
+    for i in full_text.findChildren('a'):
+        linkurl = i.attrs['href']
+        if domainlimiter in linkurl:
+            if "support-us" in linkurl or "about-us" in linkurl:
+                pass
+            else:
+                output.append([i.text, i.attrs['href']])
+    return output
+
+
+def get_pub_date(soup):
+    time = soup.select("time")[0]
+    time = time.attrs["datetime"].split("T")[0]
+    year, month, day = [int(y) for y in time.split("-")]
+    pubdate = str(datetime(year, month, day))
+    return pubdate
+
+
+def process_story_url(url):
+    try:
+        SENTENCE_TOKENIZER = nltk.data.load('tokenizers/punkt/english.pickle')
+        json_text = {}
+        log.info(url)
+        html = get_page(url)
+        soup = BeautifulSoup(html)
+        full_text = soup.select(".entry-content")[0]
+        json_text['timestamp'] = get_pub_date(soup)
+        json_text['url'] = url
+        json_text['headline'] = soup.select(".entry-title")[0].text
+        json_text['links'] = get_links(full_text)
+        full_text = full_text.text.encode('ascii', 'ignore')
+        sentences = SENTENCE_TOKENIZER.tokenize(full_text)
+        lines = []
+        for sentence in sentences:
+            output = proc.parse_doc(sentence)
+            lines = lines + [output]
+        json_text['lines'] = lines
+        hash_url = hashlib.sha224(url).hexdigest()
+        with open(processed_location + hash_url, "w") as hashfile:
+            print(json_text, file=hashfile)
+
+    except ValueError:
+        log.info('ValueError| {} '.format(url))
+    except IndexError:
+        log.info('IndexError| {} '.format(url))
+    except OSError:
+        log.info('OSError| {} '.format(url))
+
+
+if __name__ == '__main__':
+    urls = get_all_urls()
+    for url in urls:
+        process_story_url(url)
