1ed2c80feda3254330277619fd9b279282da8682
AbeHandler <akh2103@gmail.com>
2015-06-15 14:25:13 -0500

    adding consolidator

--- rookie/datamanagement/lensconsolidator.py
+++ rookie/datamanagement/lensconsolidator.py
@@ -1,9 +1,10 @@
 import ner
 import json
 import glob
+import re
 
 
-to_process = glob.glob('/Volumes/USB 1/lens_processed/*')
+TO_PROCESS = glob.glob('/Volumes/USB 1/lens_processed/*')
 
 
 def get_full_text(data):
@@ -31,7 +32,22 @@ def get_ner(data):
         for m in mentions:
             token = " ".join(tokens[m['tokspan'][0]:m['tokspan'][1]])
             ner_type = m['type']
-            output.append([token, ner_type])
+            if ner_type == 'ORDINAL' or ner_type == 'NUMBER':
+                pass
+            if ner_type == 'DATE':
+                try:
+                    valu = re.findall("value=\".+\"", m['timex_xml'])[0]
+                    valu = valu.replace('value=', '').replace('"', "")
+                    if not any(char.isdigit() for char in valu):
+                        pass
+                    elif "temporalFunction=true" in valu or "WXX" in valu:
+                        pass
+                    else:
+                        output.append([valu, ner_type])
+                except KeyError:
+                    pass
+            else:
+                output.append([token, ner_type])
     return output
 
 
@@ -51,22 +67,31 @@ def get_co_references(data):
         for mention in coref:
             sentence = mention[0]
             span = mention[1]
-            coref_group.append(" ".join(sentences[sentence]['tokens'][span[0]: span[1]]))
+            token = " ".join(sentences[sentence]['tokens'][span[0]: span[1]])
+            coref_group.append(token)
         groups.append(coref_group)
     return groups
 
 
-def correct_ner(ner, corefs):
+def correct_ner_to_first_mention(ner, corefs):
     for n in ner:
         for c in corefs:
-            if n[0] in c and n[0] != c[0] and len(c[0]) < 30 and (str(n[1])=='ORGANIZATION' or str(n[1])=='PERSON'):
+            if n[0] in c and n[0] != c[0] and len(c[0]) < 30 and \
+                      (str(n[1]) == 'ORGANIZATION' or str(n[1]) == 'PERSON'):
                 n[0] = c[0]
     return ner
 
 
-for p in to_process:
+def correct_dates(ner, timestamp):
+    for n in ner:
+        if n[1] == "DATE" and "XXXX" in n[0]:
+            n[0] == timestamp  # if XXXX for yr, just guess pub year
+    return ner
+
+
+for process_file in TO_PROCESS:
     try:
-        with open(p, 'r') as processed:
+        with open(process_file, 'r') as processed:
             data = json.load(processed)
             full_text = get_full_text(data)
             url = data['url']
@@ -76,8 +101,9 @@ for p in to_process:
             sentences = data['lines']['sentences']
             tokens = get_doc_tokens(data)
             ner = get_ner(data)
+            ner = correct_dates(ner, timestamp[0:4])
             corefs = get_co_references(data)
-            new_ner = correct_ner(ner, corefs)
+            new_ner = correct_ner_to_first_mention(ner, corefs)
             print new_ner
     except ValueError:
         print 've'
