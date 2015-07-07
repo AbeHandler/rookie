c7caf425e4b67a3d03172b4b2e26b0faa28492b7
AbeHandler <akh2103@gmail.com>
2015-07-07 13:30:02 -0400

    eeeeep deleteing old stuff

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-To see a demo of rookie, go to the [Lens website](http://rookie.thelensnola.org "Lens website"). This repo just holds the source code.
+To see a demo of rookie, go [here](http://rookie.thelensnola.org "here"). This repo just holds the source code.
 
-#h3 Rookie
+### Rookie
 
 Imagine you are a new reporter just assigned to a beat. Or a community activist interested in researching a certain political figure or government agency. News archives have lots of information that can help bring you up to speed. But reading thousands (or millions) of articles returned from a search enginge takes lots of time. So you'll have to read for a while before you get a big picture sense of what is going on. Rookie, an experimental search engine developed at [The Lens](http://www.thelensnola.org "The Lens"), is designed to help you with this task. 
 
--- rookie/utils.py
+++ rookie/utils.py
@@ -1,18 +1,9 @@
 import string
 import collections
-import pickle
 import json
-import math
 import re
-import itertools
-from time import gmtime, strftime
-from rookie import log
 from nltk.corpus import stopwords
-from elasticsearch import Elasticsearch
-from rookie.classes import Result
-from rookie.classes import QueryResult
 from rookie.classes import EntityCount
-from repoze.lru import lru_cache
 import datetime
 
 
@@ -30,70 +21,12 @@ def clean_whitespace(full_text):
     return full_text
 
 
-def clean_titles(entity):
-    with open("data/titles.txt", "r") as processed:
-        to_clean = [i.replace("\n", "") for i in processed.readlines()]
-        if any(c in entity for c in to_clean):
-            in_ner = entity
-            for c in to_clean:
-                entity = entity.replace(c, "").strip()
-            print "cleaned {} to {}".format(in_ner, entity)
-    return entity
-
-
-def query_results_to_bag_o_entities(results):
-    '''
-    Return a bag of entities
-    '''
-
-    bag_o_entities = {}
-
-    bag_o_keys = ['TIME', 'LOCATION', 'ORGANIZATION', 'PERSON', 'MONEY',
-                  'PERCENT', 'NUMBER', 'DATE']
-
-    for key in bag_o_keys:
-        bag_o_entities[key] = []
-
-    for r in results:
-        entities = r['_source']['entities']
-        keys = entities.keys()
-        for key in keys:
-            bag_o_entities[key] = bag_o_entities[key] + entities[key]
-
-    return bag_o_entities
-
-
 def get_stopwords():
     temp = stopwords.words("english")
     temp = temp + ['new', 'orleans', 'said', 'would', 'city', 'state',
                    'parish', 'louisiana', '', '|', 'said', 'say', 'story',
                    'we', 'cover', 'lens']
     return temp
-
-
-def standardize_ner(n):
-    n = n.lower()
-    n = clean_punctuation(n)
-    n = clean_whitespace(n)
-    n = n.strip()
-    return n
-
-
-def query_results_to_bag_o_words(results):
-    '''
-    Takes a list of elastic search results and returns
-    a bag of words
-    :param request: Result objects
-    :type request: list
-    :returns: list. Naievely Tokenized words
-    '''
-    STOPWORDS = get_stopwords()
-    bag_o_query_words = []
-    for result in results:
-        words = result['_source']['full_text'].split(" ")
-        words = [word for word in words if word not in STOPWORDS]
-        bag_o_query_words = bag_o_query_words + words
-    return bag_o_query_words
 
 
 def time_stamp_to_date(timestamp):
@@ -117,101 +50,6 @@ def get_entity_counts(entities, etype, results):
     for e in ents:
         e.timestamps = get_timestamps(e.name, etype, results)
     return ents
-
-
-@lru_cache(maxsize=10000)
-def get_corpus_counts(grams):
-    fname = "data/{}-grams.p".format(grams)
-    with open(fname, "rb") as gram_file:
-        grams = pickle.load(gram_file)
-    return grams
-
-
-def get_word_counts(results):
-    bag = query_results_to_bag_o_words(results)
-    words = collections.Counter(bag).most_common(25)
-    word_entities = []
-    for word in words:
-        timestamps = [time_stamp_to_date(r['_source']['timestamp'])
-                      for r in results if word[0]
-                      in r['_source']['full_text']]
-        word_entities.append(EntityCount(word, timestamps))
-    return word_entities
-
-
-@lru_cache(maxsize=10000)
-def get_denominator(counter_keys_len, delta):
-    return math.log(float(counter_keys_len + delta))
-
-
-# counter can be either phrase or corpus wide
-def lidstone(phrase, counter, delta):
-    try:
-        numerator = math.log(float(counter[phrase] + delta))
-        denominator = get_denominator(len(counter.keys()), delta)
-        lidstone = numerator / denominator
-        return lidstone
-    except KeyError:
-        return 0
-
-
-def get_lidstones(query_counter, corpus_counter):
-    delta = .5
-    lidstone_output = []
-    print str(len(query_counter.keys())) + " to check"
-    for phrase in query_counter.keys():
-        if all(len(i) > 0 for i in phrase):
-            p_phrase_given_corpus = lidstone(phrase, corpus_counter, delta)
-            p_phrase_given_query = lidstone(phrase, query_counter, delta)
-            if p_phrase_given_corpus == 0:
-                lidstone_output.append((phrase, 0))
-            else:
-                lidstone_output.append((phrase, p_phrase_given_query /
-                                       p_phrase_given_corpus))
-    return lidstone_output
-
-
-def list_to_counter(input):
-    trigrams = list(itertools.chain(*input))
-    trigrams = [tuple(x) for x in trigrams]
-    trigrams = collections.Counter(trigrams)
-    trigrams = collections.Counter(el for el in trigrams.elements()
-                                   if trigrams[el] > 2)
-    return trigrams
-
-
-# @lru_cache(maxsize=10000)
-def query_elasticsearch(lucene_query):
-    log.info("querying elastic search")
-    ec = Elasticsearch(sniff_on_start=True)
-    results = ec.search(index="lens",
-                        q=lucene_query,
-                        size=10000)['hits']['hits']
-    entities = query_results_to_bag_o_entities(results)
-    keys = ["PERSON", "LOCATION", "MONEY", "DATE", "ORGANIZATION"]
-    entity_dict = {}
-    for key in keys:
-        entity_dict[key] = get_entity_counts(entities, key, results)
-    results = [Result(r) for r in results]
-
-    trigrams = [r.trigrams for r in results]
-    trigrams = list_to_counter(trigrams)
-    trigrams = get_lidstones(trigrams, get_corpus_counts("3"))
-    trigrams = sorted(trigrams,
-                      key=lambda x: x[1],
-                      reverse=True)
-
-    bigrams = [r.bigrams for r in results]
-    bigrams = list_to_counter(bigrams)
-    bigrams = get_lidstones(bigrams, get_corpus_counts("2"))
-    bigrams = sorted(bigrams,
-                     key=lambda x: x[1],
-                     reverse=True)
-
-    query_result = QueryResult(bigrams, trigrams,
-                               entity_dict, results)
-    log.debug(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-    return query_result
 
 
 def clean_punctuation(input_string):
