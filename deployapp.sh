10c786c0d8ae8166ae886435d606940e15603c19
AbeHandler <akh2103@gmail.com>
2015-07-13 10:23:09 -0400

    few fixes to deploy

--- deployapp.sh
+++ deployapp.sh
@@ -1,1 +1,2 @@
 aws s3 cp index.html s3://rookielens --acl public-read
+aws s3 cp keys.csv.gz s3://rookielens/data/ --acl public-read --content-type text/plain --content-encoding gzip
--- pipeline.sh
+++ pipeline.sh
@@ -1,3 +1,4 @@
+rm keys.csv.gz
 python rookie/scripts/counter.py
 python rookie/scripts/pmi_calculator.py
 gzip keys.csv
