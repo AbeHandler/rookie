3f763b8727b4d7aea3e1156fbdecd132669447d5
AbeHandler <akh2103@gmail.com>
2015-07-13 17:20:11 -0400

    adding new stuff

--- deployapp.sh
+++ deployapp.sh
@@ -1,2 +1,4 @@
+aws s3 cp results.html s3://rookielens --acl public-read
 aws s3 cp index.html s3://rookielens --acl public-read
 aws s3 cp keys.csv.gz s3://rookielens/data/ --acl public-read --content-type text/plain --content-encoding gzip
+aws s3 cp gus.son.gz s3://rookielens/data/ --acl public-read --content-type text/plain --content-encoding gzip
--- pipeline.sh
+++ pipeline.sh
@@ -2,4 +2,5 @@ rm keys.csv.gz
 python rookie/scripts/counter.py
 python rookie/scripts/pmi_calculator.py
 gzip keys.csv
+find data/pmis -name '*json'  -exec gzip "{}" \;
 ./deployapp.sh
