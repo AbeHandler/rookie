424c2cf25f2fb465048f875cb7bdf980a0243302
AbeHandler <akh2103@gmail.com>
2015-07-13 20:19:12 -0400

    adding pmi fix again

--- rookie/scripts/pmi_calculator.py
+++ rookie/scripts/pmi_calculator.py
@@ -36,4 +36,4 @@ for joint_count in joint_counts.keys():
 for pmi in pmis:
     with (open("data/pmis/" + pmi + ".json", "w")) as jsonfile:
         pmis[pmi].sort(key=lambda x: x[1])
-        json.dump(pmis, jsonfile)
+        json.dump(pmis[pmi], jsonfile)
