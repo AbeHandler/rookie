00affe3b42991afba015edbefd8f70f59b712745
AbeHandler <akh2103@gmail.com>
2015-08-09 14:54:48 -0400

    adding moar fixes

--- rookie/static/js/rookie2.js
+++ rookie/static/js/rookie2.js
@@ -10,21 +10,21 @@ function get_results(start){
     $("#results").html("");
     $("#search_status").html("");
     var term = $("#search_bar").val();
-    $.post("/search?q=" + encodeURIComponent(term) + "&page=" + encodeURIComponent(String(start)), function(data){
-            $("#results").html(data);
-            $('.page').on("click", function(e){
-               $("#pages").html("");
-               var start_page = parseInt(this.id.replace("page_", ""));
-               get_results(start_page);
-            });
-            $(".term").on("click", function(e){
-                var term = $("#search_bar").val();
-                var url = "http://localhost:5000/results?q=" + term + "&term=" +  this.id + "&page=" + $("#pages").attr("data-current-page") + "&termtype=" + this.getAttribute("data-term-type") + "&startdate=" + $("#datepicker1").val() + "&enddate=" + $("#datepicker2").val();
-                location.href = url;
-            });
-        }
-    );
+    var url = "http://localhost:5000/results?q=" + term + "&term=" +  this.id + "&page=" + $("#pages").attr("data-current-page") + "&termtype=" + this.getAttribute("data-term-type") + "&startdate=" + $("#datepicker1").val() + "&enddate=" + $("#datepicker2").val();
+    location.href = url;
 }
+
+$('.page').on("click", function(e){
+  $("#pages").html("");
+  var start_page = parseInt(this.id.replace("page_", ""));
+  get_results(start_page);
+});
+
+$(".term").on("click", function(e){
+  var term = $("#search_bar").val();
+  var url = "http://localhost:5000/results?q=" + term + "&term=" +  this.id + "&page=" + $("#pages").attr("data-current-page") + "&termtype=" + this.getAttribute("data-term-type") + "&startdate=" + $("#datepicker1").val() + "&enddate=" + $("#datepicker2").val();
+   location.href = url;
+});
 
 //stackoverflow.com/questions/923299/how-can-i-detect-when-the-mouse-leaves-the-window
 function addEvent(obj, evt, fn) {
@@ -58,9 +58,30 @@ addEvent(document, "mouseout", function(e) {
 
 
  $('#search_button').on('click', function(){
-    get_results(1);
+    $("#results").html("");
+    $("#search_status").html("");
+    var term = $("#search_bar").val();
+    var url = "http://localhost:5000/results?q=" + term + "&page=1" 
+    window.location = url;
  });
- 
+  
+ //https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript 
+ (function($) {
+    $.QueryString = (function(a) {
+        if (a == "") return {};
+        var b = {};
+        for (var i = 0; i < a.length; ++i)
+        {
+            var p=a[i].split('=');
+            if (p.length != 2) continue;
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+        }
+        return b;
+    })(window.location.search.substr(1).split('&'))
+ })(jQuery);
+
+ $("#search_bar").val($.QueryString["q"]);
+ $("#advanced_search").on("click", function(){$(".toggler").toggle()});
 
  $('body').bind('keypress', function(e){
    if ( e.keyCode == 13 ) {
