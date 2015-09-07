a87cc2c13164cd507da0a7ef179e8f6a823c38ea
AbeHandler <akh2103@gmail.com>
2015-09-07 10:57:35 -0400

    z flip seems to be working

--- snippets/tmp.py
+++ snippets/tmp.py
@@ -1,5 +1,7 @@
 import pdb
 import pickle
+import random
+import itertools
 import numpy as np
 from rookie.classes import IncomingFile
 
@@ -21,22 +23,32 @@ inf = IncomingFile(file_loc + fn)
 
 query = ["common", "core", "gary", "robichaux"]
 
+
 '''
-Setup pseudocounts and counts
+Find the document's vocabulary
+'''
+
+all_tokens = [i.tokens for i in inf.doc.sentences]
+doc_vocab = set([o.raw.lower() for o in list(itertools.chain(*all_tokens))])
+doc_vocab = [i for i in doc_vocab]
+
+
+'''
+Setup pseudocounts and counts for doc/query LMs + sentence distributions.
 '''
 query_pseudoc = {}
 query_lm_counts = {}
-for word in vocab:
+for word in doc_vocab:
     query_pseudoc[word] = 1
     query_lm_counts[word] = 0
 
 doc_pseudoc = {}
 doc_lm_counts = {}
-for word in vocab:
+for word in doc_vocab:
     doc_pseudoc[word] = 1
     doc_lm_counts[word] = 0
 
-sentence_pseudo_counts = {'D': 1, 'Q': 1, 'G': 1}
+pi_pseudo_counts = {'D': 1, 'Q': 1, 'G': 1}
 
 lms = {}
 lms["query"] = {"counts": query_lm_counts, "pseudocounts": query_pseudoc}
@@ -57,8 +69,34 @@ def lookup_p_token(token, lm):
     numerator = lms[lm]['counts'][token] + lms[lm]['pseudocounts'][token]
     denom = sum(v for k, v in lms[lm]['counts'].items())
     denom = denom + sum(v for k, v in lms[lm]['pseudocounts'].items())
-    pdb.set_trace()
     return float(numerator)/float(denom)
+
+
+def lookup_p_lms(counts, pseudocounts):
+    denom = sum(v for k, v in counts.items())
+    denom = denom + sum(v for k, v in pseudocounts.items())
+    output = {}
+    output['Q'] = float((counts['Q'] + pseudocounts['Q'])) / denom
+    output['G'] = float((counts['G'] + pseudocounts['G'])) / denom
+    output['D'] = float((counts['D'] + pseudocounts['D'])) / denom
+    return output
+
+
+def flip_for_z(p_tokens, p_lms):
+    sources = ['G', 'Q', 'D']
+    total = 0.
+    ranges = {}
+    for source in sources:
+        old_total = total
+        source_area = p_tokens[source] * p_lms[source]
+        new_total = total + source_area
+        ranges[source] = (old_total, new_total)
+        total = new_total
+    flip = random.uniform(0, total)
+    for i in ranges.keys():
+        win_zone = ranges[i]
+        if flip >= win_zone[0] and flip <= win_zone[1]:
+            return i
 
 
 '''
@@ -80,5 +118,12 @@ for i in range(0, iterations):
         pi_counts = document[sentence]['pi_counts']
         for token_no in document[sentence]['tokens']:
             token = document[sentence]['tokens'][token_no]
-            p_corpus_lm = corpus_lm[token]['word']
-            p_query_lm = lookup_p_token(token, 'query')
+            p_tokens = {}
+            p_tokens['G'] = corpus_lm[token['word']]
+            p_tokens['Q'] = lookup_p_token(token['word'], 'query')
+            p_tokens['D'] = lookup_p_token(token['word'], 'document')
+            p_lms = lookup_p_lms(pi_counts, pi_pseudo_counts)
+            old_z = token['z']
+            new_z = flip_for_z(p_tokens, p_lms)
+            document[sentence]['tokens'][token_no]['z'] = new_z
+            pdb.set_trace()
