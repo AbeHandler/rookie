617c6a4f1810811417f6da85ac65052b9190fa82
AbeHandler <akh2103@gmail.com>
2015-09-17 11:22:02 -0400

    starting to build v3

--- experiment/views.py
+++ experiment/views.py
@@ -34,3 +34,33 @@ class Views(object):
         )
 
         return response
+
+
+    @staticmethod
+    def get_results_page_relational(query, results, tops, len_results, message, pages, lens_css, banner_css):
+        '''
+        Renders the homepage (/contracts/).
+        :param data: Data for the homepage.
+        :type data: dict
+        :returns: HTML. Rendered and ready for display to the user.
+        '''
+
+        organizations = [p[0] for p in tops['organizations']]
+        people = [(p[0], p[2]) for p in tops['people']]
+        terms = [p[0] for p in tops['terms']]
+        n_results = [r for r in results]
+        response = render_template(
+                'results2.html',
+                query=query,
+                people=people,
+                organizations=organizations,
+                terms=terms,
+                n_results=n_results,
+                pages=pages,
+                message=message,
+                number_of_documents=len_results,
+                lens_css=lens_css,
+                banner_css=banner_css
+        )
+
+        return response
