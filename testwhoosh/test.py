4f971649e6fbc413d8ccdd3517395ec382fe09ae
Abe Handler <akh2103@gmail.com>
2015-10-26 15:45:27 -0400

    addtestwho

--- testwhoosh/test.py
+++ testwhoosh/test.py
@@ -6,7 +6,7 @@ from rookie import processed_location
 import pdb
 import glob
 
-schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT)
+schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT, people=KEYWORD, organizations=KEYWORD)
 ix = create_in("indexdir", schema)
 writer = ix.writer()
 
@@ -18,10 +18,13 @@ for counter, infile in enumerate(files_to_check):
         print counter
         full_text = IncomingFile(infile).doc.full_text
         headline = unicode(IncomingFile(infile).headline)
+        people = [unicode(str(i)) for i in IncomingFile(infile).doc.people]
+        orgs = [unicode(str(i)) for i in IncomingFile(infile).doc.organizations]
+        ngrams = [unicode(str(i)) for i in IncomingFile(infile).doc.organizations]
         if len(headline) > 0 and len(full_text) > 0:
-            writer.add_document(title=headline, path=u"/" + str(counter), content=full_text)
+            writer.add_document(title=headline, path=u"/" + str(counter), content=full_text, people=people, organizations=orgs)
     except AttributeError:
-        print "ae"
+        pass
 
 
 writer.commit(mergetype=writing.CLEAR)
