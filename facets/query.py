15f98f7036e3bb6606dd0a850926481b374d2124
Abe Handler <akh2103@gmail.com>
2016-01-15 11:24:11 -0500

    adding alias tracking

--- facets/query.py
+++ facets/query.py
@@ -7,6 +7,7 @@ from pylru import lrudecorator
 from whoosh import query
 from dateutil.parser import parse
 from whoosh.index import open_dir
+from collections import defaultdict
 import ujson
 import numpy as np
 import ipdb
@@ -17,7 +18,7 @@ import redis
 import sys
 from Levenshtein import distance
 
-stops = ["the lens", "new orleans", "staff writer", "orleans parish"]
+stops = ["live blog", "#### live", "matt davis", "ariella cohen", "story report", "####", "#### live blog", "the lens", "new orleans", "staff writer", "orleans parish"]
 
 NDOCS = 3488  # how many docs are indexed in whoosh?
 
@@ -26,6 +27,10 @@ STOPTOKENS = ["new", "orleans"]
 DEBUG = True
 
 Q = sys.argv[1]
+
+N_GLOBAL = 10
+
+aliases = defaultdict(list)
 
 def debug_print(msg):
     if DEBUG:
@@ -111,7 +116,7 @@ def get_jaccard(one, two):
     return jacard
 
 
-def heuristic_cleanup(output, proposed_new_facet):
+def heuristic_cleanup(output, proposed_new_facet, dfs, decoder):
     '''
     An ugly heuristic that tries to filter out bad/meaningless facets quickly
 
@@ -130,12 +135,16 @@ def heuristic_cleanup(output, proposed_new_facet):
     debug_print("processing {}".format(proposed_new_facet))
     debug_print("thus far have:")
     debug_print(output)
+    new_this_round = None
     if proposed_new_facet.lower() in stops:
-        return output # dont and the new facet
+        return new_this_round, output # dont and the new facet
+    #pre processing issue
+    if "writer" in proposed_new_facet:
+        return new_this_round, output
     if proposed_new_facet in output:
-        return output # dont add duplicates
+        return new_this_round, output # dont add duplicates
     if get_jaccard(proposed_new_facet, Q) > .5:
-        return output # proposed new facet overlaps w/ query
+        return new_this_round, output # proposed new facet overlaps w/ query
     append = True # insert the facet after the check. assume true
     for index, facet in enumerate(output): # loop over facets thus far
         dist = distance(facet, proposed_new_facet) # get lev distance
@@ -146,40 +155,62 @@ def heuristic_cleanup(output, proposed_new_facet):
                 append = False
             elif (len(proposed_new_facet) > len(facet)):
                 debug_print("replacing {} with {}".format(output[index], proposed_new_facet))
+                aliases[proposed_new_facet] = aliases[output[index]] + [output[index]]
                 output[index] = proposed_new_facet
+                new_this_round = proposed_new_facet
                 append = False
         if get_jaccard(proposed_new_facet, facet) > .5:
             if len(proposed_new_facet) > len(facet):
                 debug_print("replacing {} with {}".format(output[index], proposed_new_facet))
+                aliases[proposed_new_facet] = aliases[output[index]] + [output[index]]
                 output[index] = proposed_new_facet
+                new_this_round = proposed_new_facet
             else:
                 debug_print("{} overlaps w/ {} but is shorter. don't append".format(proposed_new_facet, output[index]))
 
             # don't append even if new facet does not replace a given facet (in lines above). 
             # this is because new facet is too similar to some existing facet
             append = False
+        if proposed_new_facet.split(" ")[0].lower() == facet.split(" ").pop().lower():
+            joined = " ".join(facet.split(" ")[:-1]) + " " + proposed_new_facet
+            try:
+                count_joined = dfs[decoder[joined]]
+                count_original = dfs[decoder[output[index]]]
+                if count_original > count_joined * 2:
+                    pass
+                else:
+                    debug_print("parse/merge. replacing {} c.{} with {} c.{}".format(output[index], count_original, joined, count_joined))
+                    aliases[joined] = aliases[output[index]] + [output[index]]
+                    output[index] = joined
+                    new_this_round = joined
+                    append = False
+            except KeyError:
+                pass # a key error here means the facet does not occur in the corpus
 
 
     if append:
         debug_print("appending {}".format(proposed_new_facet))
         output.append(proposed_new_facet)
-    return [i for i in set(output)] #sometimes more than 1 facet will be replaced by propsed_new_facet
+        new_this_round = proposed_new_facet
+    return new_this_round, [i for i in set(output)] #sometimes more than 1 facet will be replaced by propsed_new_facet. So need set
 
-def get_facets(results, structures, facet_type, num_facets):
+def get_facets(results, structures, facet_type, num_facets, output = [], bin=False):
     '''
     get the tfidf score for each facet_type
     '''
-    start = time.time()
     tfidf = get_facet_tfidf(results, structures, facet_type)
-    output = []
-    for ii in np.argsort(tfidf)[::-1]:
+    bin_facets = []
+    for counter, ii in enumerate(np.argsort(tfidf)[::-1]):
         possible_f = structures["reverse_decoders"][facet_type][ii]
-        output = heuristic_cleanup(output, possible_f)
+        bin_facets.append(possible_f)
+        new, output = heuristic_cleanup(output, possible_f, structures["df"]["ngram"], structures["decoders"]["ngram"])
         #output = output + [possible_f]
         if len(output) == num_facets:
             break
-    debug_print("getting facets took {}".format(time.time() - start))
-    return output
+    if bin:
+        return output, bin_facets
+    else:
+        return output
 
 
 def get_facet_tfidf(results, structures, facet_type):
@@ -206,7 +237,27 @@ def filter_t(results, start, end):
 
 structures = load_all_data_structures()
 results = set(ROOKIE.query(Q))
+
 start = time.time()
-results = filter_t(results, parse("2012-01-01"), parse("2013-01-01"))
-debug_print("filter t took {}".format(time.time() - start))
-print get_facets(results, structures, "ngram", 15)
+global_facets = get_facets(results, structures, "ngram", N_GLOBAL)
+
+output_facets = {}
+
+output_facets["global"] = global_facets
+
+print global_facets
+
+#TODO: Brendan suggested taking top N and counting by year. How many of these
+# binned facets will show up in the top N? I dont think so many of them. check.
+#TODO: PMI version of facet engine
+
+# loop over bins.
+for index, yr in enumerate(xrange(2010, 2016)):
+    lresults = filter_t(results, parse("{}-01-01".format(yr)), parse("{}-01-01".format(yr+1)))
+    global_facets, bin_facets = get_facets(lresults, structures, "ngram", N_GLOBAL + index * N_GLOBAL, global_facets, bin=True)
+    output_facets[yr] = bin_facets
+
+print aliases
+# print output_facets["global"]
+# print output_facets
+debug_print("facets took {}".format(time.time() - start))
