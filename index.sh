471b74a57f78c6c50e82062359f1aadbf8a13ec3
Abe Handler <akh2103@gmail.com>
2016-01-22 10:44:20 -0500

    more purging of rookie code

--- index.sh
+++ index.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# This script builds all of the indexes rookie needs: whoosh, the meta data file and the db
+
 python whooshy/loader.py
 python facets/build_matrix.py
 python experiment/builddb.py
