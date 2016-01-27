3163f32205225c56fdb22167a9b854be198e6cb8
Abe Handler <akh2103@gmail.com>
2016-01-27 09:07:11 -0500

    fixes #100"

--- rookie_ui/doc_react/components/GlobalFacetButton.jsx
+++ rookie_ui/doc_react/components/GlobalFacetButton.jsx
@@ -29,10 +29,12 @@ module.exports = React.createClass({
             "paddingTop": 3
          };
          if (this.props.hovered){
-            divStyle.color = "rgb(0, 40, 163)";
+            divStyle.color = "#b33125";
+            divStyle.border = "1px solid #b33125";
          }
          if (this.props.selected){
             divStyle.color = "#b33125";
+            divStyle.border = "1px solid #b33125";
          }
          return (<div onMouseLeave={this.handleHoverOut.bind(this, this.props.name)} onMouseEnter={this.handleHoverIn.bind(this, this.props.name)} className="button tiny secondary" style={divStyle} name={this.props.name} onClick={this.handleClick.bind(this, this.props.name)}>
             {this.props.name}
