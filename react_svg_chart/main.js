8e6bf88ef960180328702656f62790527e435999
Abe Handler <akh2103@gmail.com>
2016-01-29 08:33:18 -0500

    more svg

--- react_svg_chart/main.js
+++ react_svg_chart/main.js
@@ -8,6 +8,14 @@ global.$ = global.jQuery = require('jquery');
 
 var Axis = require('./components/axis.jsx');
 
+var chart_bins = ['2010-1-1', '2010-2-1', '2010-3-1', '2010-4-1', '2010-5-1', '2010-6-1', '2010-7-1', '2010-8-1', '2010-9-1', '2010-10-1', '2010-11-1', '2010-12-1', '2011-1-1', '2011-2-1', '2011-3-1', '2011-4-1', '2011-5-1', '2011-6-1', '2011-7-1', '2011-8-1', '2011-9-1', '2011-10-1', '2011-11-1', '2011-12-1', '2012-1-1', '2012-2-1', '2012-3-1', '2012-4-1', '2012-5-1', '2012-6-1', '2012-7-1', '2012-8-1', '2012-9-1', '2012-10-1', '2012-11-1', '2012-12-1', '2013-1-1', '2013-2-1', '2013-3-1', '2013-4-1', '2013-5-1', '2013-6-1', '2013-7-1', '2013-8-1', '2013-9-1', '2013-10-1', '2013-11-1', '2013-12-1', '2014-1-1', '2014-2-1', '2014-3-1', '2014-4-1', '2014-5-1', '2014-6-1', '2014-7-1', '2014-8-1', '2014-9-1', '2014-10-1', '2014-11-1', '2014-12-1', '2015-1-1', '2015-2-1', '2015-3-1', '2015-4-1', '2015-5-1', '2015-6-1'];
+
+var binned_counts_q = [];
+
+_.forEach(chart_bins, function(value, key) { 
+  binned_counts_q.push(Math.floor((Math.random() * 100) + 1));
+});
+
 var Demo = React.createClass({
 
   getInitialState: function() {
@@ -35,7 +43,7 @@ var Demo = React.createClass({
 
   get_y_offset: function(i, scale){
     let height = this.get_height(i, scale);
-    return this.props.height - scale(i);
+    return this.props.height - this.props.belowchart - scale(i);
   },
 
   toggle_drag_start: function(e){
@@ -51,39 +59,52 @@ var Demo = React.createClass({
     this.setState({drag_r : false});
   },
 
+  i_to_date: function(i){
+    if (i % this.props.show_nth_tickmark == 0){
+      return this.props.keys[i];
+    } else{
+      return "";
+    }
+    
+  },
+
   render: function() {
-    let min = _.min(this.props.data);
-    let max = _.max(this.props.data);
+    let min = _.min(this.props.q_counts);
+    let max = _.max(this.props.q_counts);
     let height = this.props.height;
     let width = this.props.width;
+    let belowchart = this.props.height - (this.props.belowchart / 2);
     let lateralize = this.lateralize;
     let get_height = this.get_height;
     let get_y_offset = this.get_y_offset;
     let lateral_scale = d3.scale.linear()
-                        .domain([-1, this.props.data.length])
+                        .domain([-1, this.props.q_counts.length])
                         .range([0, width]);
     let height_scale = d3.scale.linear()
                         .domain([0, max])
-                        .range([0, height]);
+                        .range([0, this.props.height - this.props.belowchart]);
     let xAxis = d3.svg.axis()
                    .scale(height_scale);
     let cliq = this.handleclick;
-
+    let to_date = this.i_to_date;
     return (
 
         <div>
-        <svg width={this.props.width} height="400">
-        <rect onMouseMove={this.handleMouseMove} onMouseUp={this.toggle_drag_stop} y="0" x="0" opacity=".25" height={this.props.height} width={this.props.width} stroke="green" strokeWidth="4" fill="blue" />
-        {this.props.data.map(function(object, i){
+        <svg width={this.props.width} height={this.props.height}>
+        
+        {/* background for chart */}
+        <rect onMouseMove={this.handleMouseMove} onMouseUp={this.toggle_drag_stop} y="0" x="0" opacity=".25" height={this.props.height - this.props.belowchart} width={this.props.width} strokeWidth="4" fill="blue" />
+        {this.props.q_counts.map(function(object, i){
             return <rect key={i} onClick={e=>cliq(i)} y={get_y_offset(object, height_scale)} x={lateralize(i, lateral_scale)} height={get_height(object, height_scale)} width="14" stroke="green" strokeWidth="4" fill="yellow" />
         })}
-      {/* this is the axis */}
-        <line x1="0" y1="325" x2={this.props.width} y2="325" stroke="black" strokeWidth="5"/>
-        {this.props.data.map(function(object, i){
-            return <text key={i} x={lateralize(i, lateral_scale)} y="350" fill="black">{i}</text>
+         
+        {/* this is the axis */}
+        <line x1="0" y1={this.props.height - this.props.belowchart} x2={this.props.width} y2={this.props.height - this.props.belowchart} stroke="black" strokeWidth="5"/>
+        {this.props.q_counts.map(function(object, i){
+            return <text key={i} x={lateralize(i, lateral_scale)} y={belowchart} fill="black">{to_date(i)}</text>
         })}
-        <line onMouseDown={this.toggle_drag_start} x1={this.state.x} y1="0" x2={this.state.x} y2="50" stroke="black" strokeWidth="10"/>
-        <line onMouseDown={this.toggle_drag_start_r} x1={this.state.x_r} y1="0" x2={this.state.x_r} y2="50" stroke="black" strokeWidth="10"/>
+        <line onMouseDown={this.toggle_drag_start} x1={this.state.x} y1="0" x2={this.state.x} y2={this.props.height - this.props.belowchart} stroke="black" strokeWidth="10"/>
+        <line onMouseDown={this.toggle_drag_start_r} x1={this.state.x_r} y1="0" x2={this.state.x_r} y2={this.props.height - this.props.belowchart} stroke="black" strokeWidth="10"/>
         </svg>
         </div>
           
@@ -92,6 +113,6 @@ var Demo = React.createClass({
 });
 
 ReactDOM.render(
-  <Demo height="300" width="900" data={[ 100, 32, 100, 325, 230, 253, 235, 435, 346, 200, 300, 400, 500 ]} />,
+  <Demo show_nth_tickmark="10" belowchart="50" height="300" width="900" keys={chart_bins} q_counts={binned_counts_q} />,
   document.getElementById('example')
 );
