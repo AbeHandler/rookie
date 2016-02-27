43976ebf37611f93ec33616711d0e8883aca9ee8
Abe Handler <akh2103@gmail.com>
2016-02-27 18:30:18 -0500

    y axis working

--- rookie_ui/components/YAxis.jsx
+++ rookie_ui/components/YAxis.jsx
@@ -12,17 +12,17 @@ module.exports = React.createClass({
 
   render: function() {
     let xp = this.props.y_axis_width / 2;
-    console.log(this.props.max);
-    console.log(typeof this.props.max);
-    let scale = d3.scale.linear().domain(_.range(this.props.max + 1)).range(_.range(this.props.height));
-    console.log(scale);
-    let ticks = scale.ticks();
-    console.log(ticks);
+    let yend = this.props.height;
+    var scale = d3.scale.ordinal()
+	    .domain(_.range(this.props.max))
+	    .rangePoints([0, yend]);
+    let ticks = _.range(this.props.max);
+    let height = this.props.height;
     return (
          <svg width={this.props.y_axis_width} height={this.props.height}>
           <line x1={xp} y1="0" x2={xp} y2={this.props.height} stroke="black" strokeWidth="3" />
 	       {ticks.map(function(object, i){                           
-            return <text key={i} x="4" y={scale(i)} fill="black">{object}</text>
+            return <text key={i} x="4" y={height - scale(i)} fill="black">{i}</text>
             })}      
     	  </svg>
 	       )
