9df22b281229785ce98eca57d9847fe2d26ee31c
Abe Handler <akh2103@gmail.com>
2016-02-27 19:03:26 -0500

    adding in logging

--- webapp/app.py
+++ webapp/app.py
@@ -4,12 +4,11 @@ The main web app for rookie
 import ipdb
 import pylru
 import time
-import datetime
 import json
 import itertools
+import traceback
 from flask.ext.compress import Compress
-from dateutil.parser import parse
-from webapp.models import results_to_doclist, make_dataframe, get_keys, get_val_from_df, bin_dataframe, filter_results_with_binary_dataframe, results_min_max
+from webapp.models import results_to_doclist, make_dataframe, get_keys, get_val_from_df, bin_dataframe, results_min_max
 from flask import Flask
 from flask import request
 from facets.query_sparse import get_facets_for_q, load_all_data_structures
@@ -17,7 +16,7 @@ from webapp.views import Views
 from webapp.models import Models
 from webapp import IP, ROOKIE_JS, ROOKIE_CSS
 from webapp.models import get_doc_metadata
-import threading
+
 
 
 app = Flask(__name__)
@@ -81,76 +80,78 @@ def get_docs():
 @app.route('/', methods=['GET'])
 def medviz():
 
-    start = time.time()
     try:
-        params = Models.get_parameters(request)
-    except AttributeError:
-        return "<html><title>End-to-End Testing</title><html>"
+        start = time.time()
+        try:
+            params = Models.get_parameters(request)
+        except AttributeError:
+            return "<html><title>End-to-End Testing</title><html>"
 
-    if params.q is None:
-        return "<html>sdf<html>"
+        if params.q is None:
+            return "<html>sdf<html>"
 
-    results = Models.get_results(params)
+        results = Models.get_results(params)
 
-    fstart = time.time()
-    binned_facets = get_facets_for_q(params.q, results, 200, load_all_data_structures(params.corpus))
-    print "facet time = {}".format(time.time() - fstart)
+        fstart = time.time()
+        binned_facets = get_facets_for_q(params.q, results, 200, load_all_data_structures(params.corpus))
+        print "facet time = {}".format(time.time() - fstart)
 
-    #for f in facets:
-    #    cache[params.q + "##" + f] = aliases[f]
-    #    alias_table[params.q][f] = aliases[f]
+        #for f in facets:
+        #    cache[params.q + "##" + f] = aliases[f]
+        #    alias_table[params.q][f] = aliases[f]
 
-    aliases = [] # TODO
+        aliases = [] # TODO
 
-    q_pubdates = [load_all_data_structures(params.corpus)["pubdates"][r] for r in results]
+        q_pubdates = [load_all_data_structures(params.corpus)["pubdates"][r] for r in results]
 
-    binsize = "month"
+        binsize = "month"
 
-    print "early time = {}".format(time.time() - start)
+        print "early time = {}".format(time.time() - start)
 
-    all_facets = [i for i in itertools.chain(*binned_facets.values())]
+        dfstart = time.time()
+        df = make_dataframe(params.q, binned_facets["g"], results, q_pubdates, aliases)
+        df = bin_dataframe(df, binsize)
+        print "making + binning df = {}".format(time.time() - dfstart)
+        
 
-    dfstart = time.time()
-    df = make_dataframe(params.q, binned_facets["g"], results, q_pubdates, aliases)
-    df = bin_dataframe(df, binsize)
-    print "making + binning df = {}".format(time.time() - dfstart)
-    
+        chart_bins = get_keys(q_pubdates, binsize)
 
-    chart_bins = get_keys(q_pubdates, binsize)
+        q_data = [get_val_from_df(params.q, key, df, binsize) for key in chart_bins]
 
-    q_data = [get_val_from_df(params.q, key, df, binsize) for key in chart_bins]
+        facet_datas = {}
 
-    facet_datas = {}
+        chart_bins = [k + "-1" for k in chart_bins] # hacky addition of date to keys
 
-    chart_bins = [k + "-1" for k in chart_bins] # hacky addition of date to keys
+        display_bins = []
+        for key in binned_facets:
+            if key != "g":
+                display_bins.append({"key": key, "facets": binned_facets[key]})
 
-    display_bins = []
-    for key in binned_facets:
-        if key != "g":
-            display_bins.append({"key": key, "facets": binned_facets[key]})
+        ftime = time.time()
+        #m = threading.Thread(target=manager, args=(results, params, all_facets, aliases))
+        #m.start()
+        print "thread time = {}".format(time.time() - ftime)
 
-    ftime = time.time()
-    #m = threading.Thread(target=manager, args=(results, params, all_facets, aliases))
-    #m.start()
-    print "thread time = {}".format(time.time() - ftime)
+        print "all time = {}".format(time.time() - start)
 
-    print "all time = {}".format(time.time() - start)
-
-    stuff_ui_needs = {}
-    stuff_ui_needs["total_docs_for_q"] = len(results)
-    facets = {}
-    for f in binned_facets['g']:
-        facets[f] = [get_val_from_df(f, key, df, binsize) for key in chart_bins]
-    stuff_ui_needs["facet_datas"] = facets
-    
-    try:
-        min_str = min(load_all_data_structures(params.corpus)["pubdates"][r] for r in results)
-        max_str = max(load_all_data_structures(params.corpus)["pubdates"][r] for r in results)
-        stuff_ui_needs["first_story_pubdate"] = min_str.strftime("%Y-%m-%d")
-        stuff_ui_needs["last_story_pubdate"] = max_str.strftime("%Y-%m-%d")
-    except ValueError:
-        stuff_ui_needs["first_story_pubdate"] = ""
-        stuff_ui_needs["last_story_pubdate"] = ""
+        stuff_ui_needs = {}
+        stuff_ui_needs["total_docs_for_q"] = len(results)
+        facets = {}
+        for f in binned_facets['g']:
+            facets[f] = [get_val_from_df(f, key, df, binsize) for key in chart_bins]
+        stuff_ui_needs["facet_datas"] = facets
+        
+        try:
+            min_str = min(load_all_data_structures(params.corpus)["pubdates"][r] for r in results)
+            max_str = max(load_all_data_structures(params.corpus)["pubdates"][r] for r in results)
+            stuff_ui_needs["first_story_pubdate"] = min_str.strftime("%Y-%m-%d")
+            stuff_ui_needs["last_story_pubdate"] = max_str.strftime("%Y-%m-%d")
+        except ValueError:
+            stuff_ui_needs["first_story_pubdate"] = ""
+            stuff_ui_needs["last_story_pubdate"] = ""
+    except:
+        with open('log/log.txt', 'a') as f:
+            traceback.print_exc(file=f)
 
     return views.get_q_response_med(params, facet_datas, chart_bins, q_data, len(results), binsize, binned_facets['g'], params.corpus, stuff_ui_needs)
 
