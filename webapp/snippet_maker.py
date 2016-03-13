caafbe5e50b39849a9ea3e50294210277b48a39f
Abe Handler <akh2103@gmail.com>
2016-03-12 23:38:38 -0500

    summarization

--- webapp/snippet_maker.py
+++ webapp/snippet_maker.py
@@ -38,6 +38,7 @@ def get_snippet2(docid, corpus, q, f_aliases=None, taginfo=None):
             return [hsent]
 
         if hsent['has_q']:
+            #import ipdb; ipdb.set_trace()
             hsents['has_q'].append(hsent)
         if hsent['has_f']:
             hsents['has_f'].append(hsent)
