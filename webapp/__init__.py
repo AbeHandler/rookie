676c7cbcea5fe6948503f0d6d0970e03835230af
Abe Handler <akh2103@gmail.com>
2016-03-21 20:33:39 -0400

    small config changes for hobbes web server

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -9,6 +9,7 @@ configs = [i.replace("\n", "") for i in open(".rookie.pwd", "r")]
 
 ROOKIE_USER = [i.split("=")[1] for i in configs if "USER" in i].pop()
 ROOKIE_PW = [i.split("=")[1] for i in configs if "PASSWORD" in i].pop()
+IP = [i.split("=")[1] for i in configs if "IP" in i].pop()
 
 # http://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
 if socket.gethostname() == "dewey":
@@ -18,19 +19,18 @@ if socket.gethostname() == "dewey":
     ROOKIE_JS = "static/js/"
     ROOKIE_CSS = "static/css/"
     BASE_URL = "/"
-else:
+    files_location = "/Users/ahandler/research/rookie/"
+    processed_location = "/Users/ahandler/research/rookie/lens_processed"
+elif socket.gethostname() == "hobbes":
     BASE_URL = "http://hobbes.cs.umass.edu/~ahandler/www/rookie/main.cgi/"
     CORPUS_LOC = "/home/ubuntu/data/lens_processed/"
-    IP = "localhost"
+    IP = IP # read from config file
     ROOKIE_JS = "https://s3-us-west-2.amazonaws.com/rookie2/js/"
     ROOKIE_CSS = "https://s3-us-west-2.amazonaws.com/rookie2/css/"
     PG_HOST = os.environ.get('PG_PORT_5432_TCP_ADDR','localhost')
 
 
-if socket.gethostname() == 'dewey':
-    files_location = "/Users/ahandler/research/rookie/"
-    processed_location = "/Users/ahandler/research/rookie/lens_processed"
-elif 'btop2' in socket.gethostname():
+if 'btop2' in socket.gethostname():
     server_port=8000
     core_nlp_location = None
     corpus_loc = None
@@ -38,10 +38,6 @@ elif 'btop2' in socket.gethostname():
     processed_location = None
     tagger_base = None
     window_length = 30
-else:
-    files_location = "/home/app/rookie/"
-    processed_location = "/Users/abramhandler/research/rookie/data/lens_processed"
-    server_port = 80
 
 
 CONNECTION_STRING = 'postgresql://%s:%s@%s:5432/%s' % (
--- webapp/app.py
+++ webapp/app.py
@@ -84,4 +84,4 @@ def main():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host=IP, port=5000)
