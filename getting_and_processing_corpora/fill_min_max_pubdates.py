df842845b3031060156258268714b704ca2c9558
Abe Handler <akh2103@gmail.com>
2016-03-21 21:25:38 -0400

    overwrite the date min/max pd

--- getting_and_processing_corpora/fill_min_max_pubdates.py
+++ getting_and_processing_corpora/fill_min_max_pubdates.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dateutil import parser as dp
 import argparse
+import dateutil.parser
 
 
 def main():
@@ -17,14 +18,23 @@ def main():
     parser.add_argument('--corpus',
                         help='the thing in the middle of corpus/{}/raw',
                         required=True)
+
+    parser.add_argument('--min',
+                        required=False)
+    parser.add_argument('--max',
+                        required=False)
     args = parser.parse_args()
 
     engine = create_engine(CONNECTION_STRING)
     ession = sessionmaker(bind=engine)
     session = ession()
-    txt = "select * from doc_metadata"
-    mind = min([dp.parse(i[1]["pubdate"]) for i in session.execute(txt)])
-    maxd = max([dp.parse(i[1]["pubdate"]) for i in session.execute(txt)])
+    if args.min is None or args.max is None: 
+        txt = "select * from doc_metadata"
+        mind = min([dp.parse(i[1]["pubdate"]) for i in session.execute(txt)])
+        maxd = max([dp.parse(i[1]["pubdate"]) for i in session.execute(txt)])
+    else:
+        mind = dateutil.parser.parse(args.min)
+        maxd = dateutil.parser.parse(args.max)
     strg = "update corpora set first_story ='" + mind.strftime("%Y-%m-%d") + "' where corpusname='" + args.corpus + "'"
     session.execute(strg)
     strg2 = "update corpora set last_story ='" + maxd.strftime("%Y-%m-%d") + "' where corpusname='" + args.corpus + "'"
--- index.sh
+++ index.sh
@@ -7,7 +7,8 @@ dropdb 'rookie' --if-exists
 createdb -O rookie rookie
 py getting_and_processing_corpora/setupdb.py
 # python webapp/classes.py
-py getting_and_processing_corpora/fill_min_max_pubdates.py --corpus lens
+py getting_and_processing_corpora/fill_min_max_pubdates.py --corpus lens --min 1/1/2010 --max 4/1/2015
+py getting_and_processing_corpora/fill_min_max_pubdates.py --corpus gawk --min 10/1/2002 --max 4/20/2012
 python getting_and_processing_corpora/load_to_whoosh.py --corpus lens
 python getting_and_processing_corpora/load_to_whoosh.py --corpus gawk
 python facets/build_sparse_matrix.py --corpus lens
