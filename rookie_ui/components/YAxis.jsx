3ed3f62d1d8207f249e04260245e1fe2d781725d
Abe Handler <akh2103@gmail.com>
2016-03-24 20:18:26 -0400

    fix #166

--- rookie_ui/components/YAxis.jsx
+++ rookie_ui/components/YAxis.jsx
@@ -7,6 +7,7 @@ var React = require('react');
 var d3 = require('d3');
 var _ = require('lodash');
 var moment = require('moment');
+var ReactDOM = require('react-dom');
 
 module.exports = React.createClass({
 
@@ -20,13 +21,22 @@ module.exports = React.createClass({
     if (this.props.max<7){
 	ticks = scale.ticks(this.props.max);
     }else{
-	ticks = scale.ticks(); //_.range(this.props.max);
+        let nticks = Math.floor(this.props.height/22); //fontsize * 2 == 22
+	ticks = scale.ticks(Math.floor(this.props.height/(11 * 2))); //_.range(this.props.max);
     }let height = this.props.height;
+    
+    try {
+       console.log(Math.floor(this.props.height/11));
+    }
+    catch(err) {
+       console.log("d");
+    }
+    
     return (
          <svg width={this.props.y_axis_width} height={this.props.height}>
-          <line x1={xp} y1="0" x2={xp} y2={this.props.height} stroke="black" strokeWidth="3" />
+          <line ref="yaxis" x1={xp} y1="0" x2={xp} y2={this.props.height} stroke="black" strokeWidth="3" />
 	       {ticks.map(function(object, i){                           
-            return <text key={i} x="4" y={yend - scale(object)} fill="black">{object}</text>
+            return <text fontSize="11" key={i} x="4" y={yend - scale(object)} fill="black">{object}</text>
             })}      
     	  </svg>
 	       )
