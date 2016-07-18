0144345a781dcdc147ad743707c4b9c68b7cd1b9
Abe Handler <akh2103@gmail.com>
2016-07-18 12:32:45 -0400

    adding countrymaker

---
+++ getting_and_processing_corpora/countrymaker.py
@@ -0,0 +1,62 @@
+import json
+import ipdb
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='args')
+
+ANNO_LOC = "/Users/ahandler/research/posphrases/py/"
+
+parser.add_argument('-country', action="store")
+
+args = parser.parse_args()
+
+country = args.country
+
+BASE = '/Users/ahandler/research/rookie/corpora/'
+
+try:
+    os.makedirs(BASE + country)
+    os.makedirs(BASE + country + "/processed")
+    os.makedirs(BASE + country + "/raw")
+except OSError:
+    pass
+
+
+def has_country(jdoc, country):
+    '''does this document mention this country?'''
+    for sent in jdoc["sentences"]:
+        for tok in sent["tokens"]:
+            if tok.lower() == country.lower():
+                return True
+    return False
+
+
+def remove(fn):
+    try:
+        os.remove(fn)
+    except OSError:
+        pass
+
+
+OUTF = BASE + "{}/processed/all.anno_plus".format(country)
+
+remove(OUTF)
+
+# nicaraguas is a list preprocessed w/ grep.
+# could easily by a list of all anno files.
+with open("nicaraguas", "r") as inf:
+    for ln in inf:
+        fn = ln.replace("\n", "")
+        if "anno_plus" in fn:
+            with open(ANNO_LOC + "/" + fn, "r") as newf:
+                for ln2 in newf:
+                    pubdate = ln2.split("\t")[0]
+                    dt = ln2.split("\t")[1]
+                    jdoc = json.loads(dt)
+                    jdoc["headline"] = "unknown"
+                    jdoc["url"] = "unknown"
+                    jdoc["pubdate"] = pubdate
+                    if has_country(jdoc, country):
+                        with open(OUTF, "a") as outf:
+                            outf.write(json.dumps(jdoc) + "\n")
